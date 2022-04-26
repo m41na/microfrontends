f83c173c513bbc1652b39cc945cf46fbc90c835f
Maina, Stephen <m41na@yahoo.com>
2022-04-25 21:02:35 -0500

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const mount = root => {
+    ReactDOM.render(
+        <App />,
+        root
+    );
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector("#_marketing-dev-root");
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+export { mount };

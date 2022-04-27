46c7cda2dfd0e492bd57d8fac8010fc4e163611d
Maina, Stephen <m41na@yahoo.com>
2022-04-26 20:04:23 -0500

    Adding css namespacing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
@@ -10,13 +10,15 @@ const generateClassName = createGenerateClassName({
 
 export default () => {
     return (
-        <BrowserRouter>
+        <div>
             <StylesProvider generateClassName={generateClassName}>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path="/" component={Landing} />
-                </Switch>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/pricing" component={Pricing} />
+                        <Route path="/" component={Landing} />
+                    </Switch>
+                </BrowserRouter>
             </StylesProvider>
-        </BrowserRouter>
+        </div>
     );
 }
